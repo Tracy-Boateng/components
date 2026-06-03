2cd36572e3f90ecb312754c2cb894f783d2dcb5a
Tracy <trac.boats.tb@gmail.com>
2026-06-03 16:42:38 -0500

    header.js / app.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,18 @@
 function Header () {
     return (
-        <></>
+        <header>
+            <h1>HomeBnB</h1>
+
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Rooms</li>
+                    <li>Experiences</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+
     );
 }
 
